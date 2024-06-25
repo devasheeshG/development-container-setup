81f16cc90a024ffc92b38c911451ce6d8defd45a
Devasheesh Mishra <devasheeshmishra4@gmail.com>
2024-06-26 02:50:40 +0530

    build docker image

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,1 +1,10 @@
-docker build -t ubuntu22.04-devel-cuda12.1-cudnn8:latest .
+#!/bin/bash
+
+# Load environment variables from .env file
+# export $(cat .env | xargs)
+
+# Build the Docker image with --build-arg
+docker build . \
+  # --build-arg ROOT_PASSWORD=${ROOT_PASSWORD} \
+  # --build-arg DEVASHEESH_PASSWORD=${DEVASHEESH_PASSWORD} \
+  -t ubuntu22.04-devel-cuda12.1-cudnn8:latest .
